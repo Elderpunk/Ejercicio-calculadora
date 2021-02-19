bddfe9646facf73a78508c2d01ddcf55c1c45109
jose <gonzaleztroyajc@gmail.com>
2021-02-19 19:02:10 +0100

    Incluido JS

--- calculadora.js
+++ calculadora.js
@@ -1,26 +1,25 @@
+//Creamos arreglos y variables
+
 const botonNumeros = document.getElementsByName('numero')
-
 const botonOperadores = document.getElementsByName('operaciones')
-
 const botonIgual = document.getElementsByName('igual')[0]
-
 const botonBorrar = document.getElementsByName('borrar')[0]
-
-let resultado = document.getElementById('resultado')
-
-let operacionActual =''
-let operacionAnterior =''
+let result = document.getElementById('resultado')
+let operacionActual = ''
+let operacionAnterior = ''
 let operacion = undefined
 
-botonNumeros.forEach(function (boton){
-    boton.addEventListener('click', function(){
-        introducirNumero(boton.innerText)
+//Agregamos eventos para llamar metodos
+
+botonNumeros.forEach(function(boton){
+    boton.addEventListener('click',function(){
+        insertarNumero(boton.innerText)
     })
 })
 
-botonOperadores.forEach(function (boton){
-    boton.addEventListener('click', function(){
-        seleccionaOperacion(boton.innerText)
+botonOperadores.forEach(function(boton){
+    boton.addEventListener('click',function(){
+        seleccioneOperacion(boton.innerText)
     })
 })
 
@@ -30,23 +29,59 @@ botonIgual.addEventListener('click', function(){
 })
 
 botonBorrar.addEventListener('click', function(){
-    limpiarPantalla()
+    clear()
     actualizarDisplay()
 })
+    
+//Implementamos los metodos
+function seleccioneOperacion(op){
+    if(operacionActual === '')return
+    if(operacionAnterior !== ''){
+        calcular()
+    }
+    operacion = op.toString()
+    operacionAnterior = operacionActual
+    operacionActual = ''
+}
 
-function introducirNumero(num){
+function calcular(){
+    let calculo
+    const anterior = parseFloat(operacionAnterior)
+    const actual = parseFloat(operacionActual)
+    if(isNaN(anterior) || isNaN(actual))return
+    switch (operacion) {
+        case '+':
+            calculo = anterior + actual
+            break
+            case '-':
+            calculo = anterior - actual
+            break
+            case 'x':
+                calculo = anterior * actual
+                break
+                case '/':
+            calculo = anterior / actual
+            break;
+        default:return
+            
+    }
+    operacionActual = calculo 
+    operacion = undefined
+    operacionAnterior = ''
+}
+
+function insertarNumero(num){
     operacionActual = operacionActual.toString() + num.toString()
     actualizarDisplay()
 }
 
-function limpiarPantalla(){
-    operacionActual = ""
-    operacionAnterior = ''
+function clear(){
+    operacionActual = ''
+    operacionAnterior = ''                                              
     operacion = undefined
-}
-
+}                                                                   
 function actualizarDisplay(){
     resultado.value = operacionActual
 }
 
-limpiarPantalla()
+clear()
